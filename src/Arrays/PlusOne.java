5d88efcf00339dd98b24212ee7a788f377c9b7a8
= <=>
2024-05-28 07:54:14 -0400

    Added PlusOne problem solution

--- src/Arrays/PlusOne.java
+++ src/Arrays/PlusOne.java
@@ -3,39 +3,28 @@ package Arrays;
 import java.util.Arrays;
 
 public class PlusOne {
-    public static  int[] plusOne(int[] digits) {
-        int integer = 0;
-        if(digits.length != 1){
-            for(int i =0; i< digits.length -1; i++){
-                integer = integer * 10 + digits[i];
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-            integer = integer*10 + digits[digits.length-1]+1;
-            int[] arr = new int[digits.length];
-            for(int i =digits.length -1;i>=0;i--){
-                arr[i] = integer % 10;
-                integer = integer / 10;
-            }
-            return arr;
-        }
-        integer =  digits[digits.length-1]  + 1;
-        System.out.println(integer);
-        int[] arr = new int[digits.length +1];
-        for(int i =arr.length -1;i>=0;i--){
-            arr[i] = integer % 10;
-            integer = integer / 10;
-        }
+            digits[i] = 0;
 
-
-        return arr;
+        }
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
 
     }
+
     public static void main(String[] args) {
-        int[] digits = {0};
-        int [] arr = plusOne(digits);
-        for(int i : arr){
+
+        int[] digits = {9, 9};
+        int[] arr = plusOne(digits);
+        for (int i : arr) {
             System.out.println(i);
         }
-
 
 
     }
